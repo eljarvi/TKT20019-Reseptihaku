60f7ed4698ee5a69c14764b610c10d8fe4457f80
Ellen <jarvi.ellen@gmail.com>
2023-11-28 19:06:30 +0200

    Add reviews.py

---
+++ reviews.py
@@ -0,0 +1,16 @@
+from db import db
+from sqlalchemy.sql import text
+
+def add_review(user_id, recipe_id, text, grade):
+    sql = "INSERT INTO Reviews (user_id, recipe_id, review, grade, visible) \
+            VALUES (:user_id, :recipe_id, :text, :grade) RETURNING id"
+    review_id = db.session.execute(
+                text(sql),
+                {"user_id": user_id, "recipe_id": recipe_id, "text": text, "grade": grade}
+                ).fetchone()[0]
+    return review_id
+
+def review_details(review_id):
+    sql = "SELECT user_id, recipe_id, review, grade \
+            FROM Reviews WHERE id = :review_id"
+    return db.session.execute(text(sql), {"review_id": review_id}).fetchone()
