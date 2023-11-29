61c609c7db42c6971162edc8a1a1b14e7bfd97da
Ellen <jarvi.ellen@gmail.com>
2023-11-29 16:44:40 +0200

    Add recipe_reviews()

--- reviews.py
+++ reviews.py
@@ -5,9 +5,9 @@ def add_review(user_id, recipe_id, review, grade):
     sql = "INSERT INTO Reviews (user_id, recipe_id, review, grade, visible) \
             VALUES (:user_id, :recipe_id, :review, :grade, true) RETURNING id"
     db.session.execute(
-                text(sql),
-                {"user_id": user_id, "recipe_id": recipe_id, "review": review, "grade": grade}
-                )
+            text(sql),
+            {"user_id": user_id, "recipe_id": recipe_id, "review": review, "grade": grade}
+            )
     db.session.commit()
 
 def review_details(review_id):
@@ -18,6 +18,15 @@ def review_details(review_id):
 def have_reviewed(user_id, recipe_id):
     sql = "SELECT COUNT(id) FROM Reviews WHERE visible \
             AND user_id = :user_id AND recipe_id = :recipe_id"
-    result = db.session.execute(text(sql), {"user_id": user_id, "recipe_id": recipe_id}).fetchone()[0]
+    result = db.session.execute(
+        text(sql),
+        {"user_id": user_id, "recipe_id": recipe_id}
+        ).fetchone()[0]
     return result >= 1
+
+def recipe_reviews(recipe_id):
+    sql = "SELECT id, user_id, review, grade FROM Reviews \
+            WHERE visible AND recipe_id = :recipe_id"
+    return db.session.execute(text(sql), {"recipe_id": recipe_id}).fetchall()
+
 
